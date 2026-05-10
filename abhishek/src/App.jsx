7ceba228e44d3ebef0f817bcd7d6e447e7557c32
AbhishekSingh321 <singhabhi292004@gmail.com>
2026-05-10 05:44:15 +0530

    feat: implement 3D interactive lanyard component using React Three Fiber and Rapier physics

--- abhishek/src/App.jsx
+++ abhishek/src/App.jsx
@@ -1,10 +1,14 @@
-
+import './App.css';
+import Lanyard from './components/Lanyard/Lanyard';
 
 function App() {
 
   return (
     <>
       <h1>Hello I'm Abhishek 👋🏻</h1>
+      <div className="lanyard-container">
+        <Lanyard position={[0, 0, 20]} gravity={[0, -40, 0]} />
+      </div>
     </>
   )
 }
